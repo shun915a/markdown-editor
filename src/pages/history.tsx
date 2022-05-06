47bccab632dff4c706a2387cb1b3aff468fc86ca
shun915a <shun915a@gmail.com>
2022-05-06 11:41:17 +0900

    history function

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -29,9 +29,31 @@ const Wrapper = styled.div`
   padding: 0 1rem;
 `
 
+const Memo = styled.button`
+  display: block;
+  background-color: white;
+  border: 1px solid gray;
+  width: 100%;
+  padding: 1rem;
+  margin: 1rem 0;
+  text-align: left;
+`
+
+const MemoTitle = styled.div`
+  font-size: 1rem;
+  margin-bottom: 0.5rem;
+`
+
+const MemoText = styled.div`
+  font-size: 0.85rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
+
+
 export const History: React.FC = () => {
   const [memos, setMemos] = useState<MemoRecord[]>([])
-  console.log(memos)
 
   useEffect(() => {
     getMemos().then(setMemos)
@@ -47,7 +69,12 @@ export const History: React.FC = () => {
         </Header>
       </HeaderArea>
       <Wrapper>
-        TODO: 履歴表示
+        {memos.map(memo => (
+          <Memo key={memo.datetime}>
+            <MemoTitle>{memo.title}</MemoTitle>
+            <MemoText>{memo.text}</MemoText>
+          </Memo>
+        ))}
       </Wrapper>
     </>
   )
